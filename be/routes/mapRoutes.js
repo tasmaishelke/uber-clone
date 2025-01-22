b77f323565e5254071e6088952cd8405f6a2a88b
tasmai <tasmaishelke@gmail.com>
2025-01-22 11:43:06 +0530

    added maps api functions

--- be/routes/mapRoutes.js
+++ be/routes/mapRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = express.Router();
 
-const { getAddressCoordinate } = require('../services/mapService')
+const { getAddressCoordinate, getDistanceTime, getSuggestion } = require('../services/mapService')
 
 // middlewares
 const { authUser } = require('../middlewares/authMiddleware')
@@ -23,13 +23,65 @@ router.get('/get-coordinate',
             const { address } = req.query;
             try
                 {
-                    const coordinates = await getAddressCoordinate(address);
-                    res.status(200).json(coordinates)
+                    const coordinate = await getAddressCoordinate(address);
+                    res.status(200).json(coordinate)
                 }
             catch(error)
                 {
                     res.status(404).json({ Message : "Coordinate not found"})
                 }
         })
+
+
+router.get('/get-distance-time',
+    [
+        query('origin').isString().isLength({ min: 3 }),
+        query('destination').isString().isLength({ min: 3 })
+    ],
+    authUser,
+    async(req, res, next) =>
+        {
+            const errors = validationResult(req)
+            if(!errors.isEmpty())
+                {
+                    return res.status(400).json({ errors : errors.array()});
+                }
+            const { origin, destination } = req.query;
+            try
+                {
+                    const distanceTime = await getDistanceTime(origin, destination);
+                    res.status(200).json(distanceTime)
+                }
+            catch(error)
+                {
+                    res.status(404).json({ Message : "Distance and time not found"})
+                }
+        }
+)
+
+router.get('/get-suggestion',
+    [
+        query('input').isString().isLength({ min: 2 }),
+    ],
+    authUser,
+    async(req, res, next) =>
+        {
+            const errors = validationResult(req)
+            if(!errors.isEmpty())
+                {
+                    return res.status(400).json({ errors : errors.array()});
+                }
+            const { input } = req.query;
+            try
+                {
+                    const suggestion = await getSuggestion(input);
+                    res.status(200).json(suggestion)
+                }
+            catch(error)
+                {
+                    res.status(404).json({ Message : "Suggestions not found"})
+                }
+        }
+)
 
 module.exports = router
--- be/services/mapService.js
+++ be/services/mapService.js
@@ -25,7 +25,59 @@ const getAddressCoordinate = async(address) =>
             {
                 console.log(error);
                 throw error;                
-            }        
+            }
     }
 
-module.exports = { getAddressCoordinate }
+const getDistanceTime = async(origin, destination) =>
+    {
+        const apiKey = process.env.GOOGLE_MAPS_API;
+        const url = `https://maps.gomaps.pro/maps/api/distancematrix/json?origins=${encodeURIComponent(origin)}&destinations=${encodeURIComponent(destination)}&key=${apiKey}`;
+
+        try
+            {
+                const res = await axios.get(url);
+                if(res.data.status === 'OK')
+                    {
+                        if(res.data.rows[0].elements[0].status === 'ZERO_RESULTS')
+                            {
+                                throw new Error('No routes found')
+                            }
+                        return res.data.rows[0].elements[0]
+                    }
+                else
+                    {
+                        throw new Error('Unable to fetch distance and time');
+                    }
+            }
+        catch(error)
+            {
+                console.log(error);
+                throw error;                
+            }
+    }
+
+const getSuggestion = async(input) =>
+    {
+        const apiKey = process.env.GOOGLE_MAPS_API;
+        const url = `https://maps.gomaps.pro/maps/api/place/autocomplete/json?input=${encodeURIComponent(input)}&key=${apiKey}`;
+
+        try
+            {
+                const res = await axios.get(url);
+                if(res.data.status === 'OK')
+                    {
+                        return res.data.predictions
+                    }
+                else
+                    {
+                        throw new Error('Unable to fetch suggestions');
+                    }
+            }
+        catch(error)
+            {
+                console.log(error);
+                throw error;                
+            }
+    }
+
+module.exports = { getAddressCoordinate, getDistanceTime, getSuggestion }
